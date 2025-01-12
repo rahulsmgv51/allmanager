f10a7bc9f8889179f2a77a74e146291c80a6ed56
rahulsmgv51 <rahulsmgv@gmail.com>
2025-01-12 17:14:15 +0530

    Project Base code

---
+++ src/main/java/com/rahulsmgv/allmanager/AllmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.rahulsmgv.allmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahulsmgv/allmanager/AllmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahulsmgv.allmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
